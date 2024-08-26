c64377f8463b57ed346709b570f70a7672348c30
Lum-ona <lumonamulengwa@gmail.com>
2024-08-26 11:36:10 +0300

    added preloader

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/home/Home";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import About from "./pages/about/About";
@@ -9,6 +9,7 @@ import CurrentAffairs from "./pages/currentAffairs/CurrentAffairs";
 import Donate from "./pages/donate/Donate";
 import ProgramDirectory from "./pages/program/ProgramDirectory";
 import Contact from "./pages/contact/Contact";
+import Preloader from "./components/preloader/Preloader";
 
 function App() {
   function ScrollToTopOnRouteChange() {
@@ -21,30 +22,46 @@ function App() {
     return null;
   }
 
+  const [loading, setLoading] = useState(true); // State to manage the loading status
+
+  useEffect(() => {
+    // Simulate loading process
+    setTimeout(() => {
+      setLoading(false); // Set loading to false after 2 seconds (simulate data fetching)
+    }, 2000);
+  }, []);
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Header />
-        <ScrollToTopOnRouteChange />
-        <main>
-          <Routes>
-            <Route index path="/" element={<Home />} />
-            <Route index path="/about/:id" element={<About />} />
-            <Route
-              index
-              path="/program-directory/:id"
-              element={<ProgramDirectory />}
-            />
-            <Route
-              index
-              path="/current-affairs/:categoryId/:itemsId?"
-              element={<CurrentAffairs />}
-            />
-            <Route index path="/donate" element={<Donate />} />
-            <Route index path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        <Footer />
+        {loading ? (
+          <Preloader />
+        ) : (
+          <>
+            {" "}
+            <Header />
+            <ScrollToTopOnRouteChange />
+            <main>
+              <Routes>
+                <Route index path="/" element={<Home />} />
+                <Route index path="/about/:id" element={<About />} />
+                <Route
+                  index
+                  path="/program-directory/:id"
+                  element={<ProgramDirectory />}
+                />
+                <Route
+                  index
+                  path="/current-affairs/:categoryId/:itemsId?"
+                  element={<CurrentAffairs />}
+                />
+                <Route index path="/donate" element={<Donate />} />
+                <Route index path="/contact" element={<Contact />} />
+              </Routes>
+            </main>
+            <Footer />
+          </>
+        )}
       </BrowserRouter>
     </div>
   );
